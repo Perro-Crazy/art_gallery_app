dd7cd47e0a3f3bc2f961730998da301a4ab770ae
mvuka <mvuka.app@gmail.com>
2024-08-12 18:51:55 -0300

    create application

---
+++ app/src/main/java/com/mauri/movieapp/foundation/App.kt
@@ -0,0 +1,15 @@
+package com.mauri.movieapp.foundation
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            modules(DI.modules)
+        }
+    }
+}
