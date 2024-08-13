39e195576be6572a1d1d1a4c79f1a0df206bf163
mvuka <mvuka.app@gmail.com>
2024-08-13 09:48:38 -0300

    add image

--- app/src/main/java/com/mauri/movieapp/presentation/feature/artlist/ArtListScreen.kt
+++ app/src/main/java/com/mauri/movieapp/presentation/feature/artlist/ArtListScreen.kt
@@ -1,34 +1,25 @@
 package com.mauri.movieapp.presentation.feature.artlist
 
-import android.graphics.BitmapFactory
-import android.graphics.ImageDecoder
-import android.os.Build
-import android.util.Base64
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import coil.ImageLoader
-import coil.ImageLoaderFactory
 import coil.compose.AsyncImage
-import coil.decode.GifDecoder
-import coil.decode.ImageDecoderDecoder
-import coil.request.ImageRequest
 
 
 object ArtListScreen {
@@ -42,16 +33,27 @@ object ArtListScreen {
 
     @Composable
     private fun SuccessRender(state: ArtListViewModel.State.Success) {
-        LazyColumn(
-            modifier = Modifier.fillMaxSize()
-        ) {
+        LazyColumn {
             items(state.data.size, itemContent = {
                 state.data[it].run {
-                    Text(text = title)
-                    AsyncImage(
-                        model = image,
-                        contentDescription = null
-                    )
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxSize().padding(10.dp)
+                    ) {
+                        AsyncImage(
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .size(64.dp)
+                                .clip(CircleShape)
+                                .border(1.dp, Color.Blue, CircleShape),
+                            model = image,
+                            contentDescription = null
+                        )
+                        Text(
+                            text = title,
+                            modifier = Modifier.fillMaxWidth().padding(10.dp)
+                        )
+                    }
                 }
             })
         }
