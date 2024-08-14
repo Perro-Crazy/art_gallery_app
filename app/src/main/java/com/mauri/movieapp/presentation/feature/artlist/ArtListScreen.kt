abbf08da26ddb56a5c2f08991e65dd1ef16b481b
mvuka <mvuka.app@gmail.com>
2024-08-14 12:51:13 -0300

    review persistence state

--- app/src/main/java/com/mauri/movieapp/presentation/feature/artlist/ArtListScreen.kt
+++ app/src/main/java/com/mauri/movieapp/presentation/feature/artlist/ArtListScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
